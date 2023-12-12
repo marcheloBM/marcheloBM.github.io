cb05b6b60a1aaa8bb3d7de980bdf1f9d73ae2af1
Marcelo Burgos <marchelo.bm@gmail.com>
2023-12-12 02:40:39 -0300

    v1.28 Nuevo Tutorial PS2
    Nuevo tutorial de PS2 y cambios menores al codigo de de programa y linkPS2

---
+++ Home/js/linkPS2.js
@@ -0,0 +1,132 @@
+function PS2ApolloSaveTool(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://mega.nz/folder/qVIWlCLR#kZnXtrmBdxj9hj6ZEk1svw")
+	}else{
+        IniciarSession();
+	}
+}
+function PS2ApolloSaveToolWeb(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://github.com/bucanero/apollo-ps2")
+	}else{
+        IniciarSession();
+	}
+}
+function FreeDVDBoot(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://mega.nz/folder/eNJU2ACR#dfkKW8hHc-FwgCOz3C4sJA")
+	}else{
+        IniciarSession();
+	}
+}
+function FreeDVDBootWeb(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://github.com/CTurt/FreeDVDBoot")
+	}else{
+        IniciarSession();
+	}
+}
+function FreeMemoryCardBoot(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://mega.nz/folder/iMwQTSxQ#dVtETs8KUVYrxKX5-8Q9bw")
+	}else{
+        IniciarSession();
+	}
+}
+function FreeMemoryCardBootWeb(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://www.psx-place.com/resources/free-memory-card-boot-fmcb-free-harddisk-drive-boot-fhdb-by-sp193.712/updates")
+	}else{
+        IniciarSession();
+	}
+}
+function FunTuna(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://mega.nz/folder/iVJUmRiS#UZnePqmhyeFU_YYk_Nk6Eg")
+	}else{
+        IniciarSession();
+	}
+}
+function FunTunaWeb(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://github.com/israpps/Funtuna-Fork")
+	}else{
+        IniciarSession();
+	}
+}
+function OpenPS2Loader(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://mega.nz/folder/7c4CCYgT#3Am-DMOcMZpO75t7f4vOhQ")
+	}else{
+        IniciarSession();
+	}
+}
+function OpenPS2LoaderWeb(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://github.com/ps2homebrew/Open-PS2-Loader")
+	}else{
+        IniciarSession();
+	}
+}
+function Aplicaciones(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://mega.nz/file/eARDUaQa#sgEheVugmdVlfzWu0zlsPq629lHV3SdBPWLV0Zwz7Eo")
+	}else{
+        IniciarSession();
+	}
+}
+function MEGAPACKAPPSPS2(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("https://mega.nz/file/CJIhnBib#JZT6g3lws7VsIqorpKgDrgDNCdgaJMMp37hIrwoeAOg")
+	}else{
+        IniciarSession();
+	}
+}
+
+
+
+
+
+var dato1 = sessionStorage.Usuario;
+function IniciarSession(){
+    var resultado = window.confirm('Sesión no iniciada.\nDesea iniciar sesión?');
+        if (resultado === true) {
+            window.alert('Ingrese los datos a continuación');
+            window.location.href='../Login.html';
+        } else { 
+            window.alert('session no iniciada');
+            
+        }
+}
+
+
+
+
+function nombre(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("")
+	}else{
+        IniciarSession();
+	}
+}
+function Web(){
+	if (dato1 == "marchelo") {
+        //validamos si alguna sesión activa
+        window.open("")
+	}else{
+        IniciarSession();
+	}
+}
