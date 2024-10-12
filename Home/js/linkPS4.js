83119d2eb6f091aa07fa773942cd74c0dd7b5be9
Marcelo Burgos <marchelo.bm@gmail.com>
2024-10-11 23:57:13 -0300

    v1.78 Nuevos Link PS4
    Se agrego nuevos Link para futuros tutoriales de PS4

--- Home/js/linkPS4.js
+++ Home/js/linkPS4.js
@@ -1,5 +1,20 @@
 function PS4Update(){verificarSesionYAbrirEnlace("https://mega.nz/folder/nARQlDgB#Py2-_d7txCc1AbeLV9M7uQ");}
 function PS4UpdateWeb(){verificarSesionYAbrirEnlace("https://www.playstation.com/es-cl/support/hardware/ps4/system-software/");}
+function PS4UpdateWebAllWeb(){verificarSesionYAbrirEnlace("https://darthsternie.net/ps4-firmwares/");}
+function USB900(){verificarSesionYAbrirEnlace("https://mega.nz/folder/yMpEnCgK#7Td3FGNBx1sOIfX0woKQWQ");}
+function USB900Web(){verificarSesionYAbrirEnlace("https://github.com/ChendoChap/pOOBs4");}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
