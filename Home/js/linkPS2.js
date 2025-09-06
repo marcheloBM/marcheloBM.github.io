46b1e21afc9823842aebfb3bcbf4850ddee93d4e
Marcelo Burgos <marchelo.bm@gmail.com>
2025-09-06 06:24:34 -0400

    v2.01 Nuevos Link de descargas PS2
    Se cambio los link de descargas de PS2

--- Home/js/linkPS2.js
+++ Home/js/linkPS2.js
@@ -1,33 +1,34 @@
-function PS2ApolloSaveTool(){verificarSesionYAbrirEnlace("https://mega.nz/folder/qVIWlCLR#kZnXtrmBdxj9hj6ZEk1svw");}
-function PS2ApolloSaveToolWeb(){verificarSesionYAbrirEnlace("https://github.com/bucanero/apollo-ps2");}
-function FreeDVDBoot(){verificarSesionYAbrirEnlace("https://mega.nz/folder/eNJU2ACR#dfkKW8hHc-FwgCOz3C4sJA");}
+function FreeDVDBoot(){verificarSesionYAbrirEnlace("https://mega.nz/folder/OJQ3wJwT#NVGgoTWrqUyf_ILKqDQ5zw");}
 function FreeDVDBootWeb(){verificarSesionYAbrirEnlace("https://github.com/CTurt/FreeDVDBoot");}
-function FreeMemoryCardBoot(){verificarSesionYAbrirEnlace("https://mega.nz/folder/iMwQTSxQ#dVtETs8KUVYrxKX5-8Q9bw");}
+function FreeMemoryCardBoot(){verificarSesionYAbrirEnlace("https://mega.nz/folder/SVAWTTLb#dOt2fvuBsnqGFpqW7jZSYw");}
 function FreeMemoryCardBootWeb(){verificarSesionYAbrirEnlace("https://www.psx-place.com/resources/free-memory-card-boot-fmcb-free-harddisk-drive-boot-fhdb-by-sp193.712/updates");}
-function FunTuna(){verificarSesionYAbrirEnlace("https://mega.nz/folder/iVJUmRiS#UZnePqmhyeFU_YYk_Nk6Eg");}
+function FunTuna(){verificarSesionYAbrirEnlace("https://mega.nz/folder/LYITyTjJ#V-WgMLYoAtrs0BY4V6p1Ow");}
 function FunTunaWeb(){verificarSesionYAbrirEnlace("https://github.com/israpps/Funtuna-Fork");}
-function OpenPS2Loader(){verificarSesionYAbrirEnlace("https://mega.nz/folder/7c4CCYgT#3Am-DMOcMZpO75t7f4vOhQ");}
+function PS2ApolloSaveTool(){verificarSesionYAbrirEnlace("https://mega.nz/folder/CY5UyCwR#Me7K6UpAD3u2-QWpQnmDZA");}
+function PS2ApolloSaveToolWeb(){verificarSesionYAbrirEnlace("https://github.com/bucanero/apollo-ps2");}
+function OpenPS2Loader(){verificarSesionYAbrirEnlace("https://mega.nz/folder/jRB3iT5K#tYTboCYhj6PzZlpOaQlOYg");}
 function OpenPS2LoaderWeb(){verificarSesionYAbrirEnlace("https://github.com/ps2homebrew/Open-PS2-Loader");}
-function Aplicaciones(){verificarSesionYAbrirEnlace("https://mega.nz/file/eARDUaQa#sgEheVugmdVlfzWu0zlsPq629lHV3SdBPWLV0Zwz7Eo");}
-function MEGAPACKAPPSPS2(){verificarSesionYAbrirEnlace("https://mega.nz/file/CJIhnBib#JZT6g3lws7VsIqorpKgDrgDNCdgaJMMp37hIrwoeAOg");}
-function Retroarch(){verificarSesionYAbrirEnlace("https://mega.nz/folder/nQBCUJDb#ZwGmtPJ_n83I1sEmY3SWKA");}
-function RetroarchWeb(){verificarSesionYAbrirEnlace("https://www.retroarch.com/?page=platforms");}
-function PSXaPS2(){verificarSesionYAbrirEnlace("https://mega.nz/folder/zUgEhTYJ#7EmZRfMOrpw2xZqOjXCGzg");}
-function OPLManager(){verificarSesionYAbrirEnlace("https://mega.nz/folder/PFRWhATR#bSc9f4yLBrvxELPZ8Npv6A");}
-function OPLManagerWeb(){verificarSesionYAbrirEnlace("https://oplmanager.com/site/index.php");}
-function OPLSERVER(){verificarSesionYAbrirEnlace("https://mega.nz/folder/DQQQxYwb#BB1bNQpZtbvEO3yIH2p_2g");}
-function OPLSERVERWeb(){verificarSesionYAbrirEnlace("https://github.com/elmariolo/OPL-Server");}
-function USBUTIL(){verificarSesionYAbrirEnlace("https://mega.nz/folder/PQQk0LhZ#pxeawP7k9NaMSBDvQj-Lyg");}
-function APPSUSB(){verificarSesionYAbrirEnlace("https://mega.nz/folder/uRwVhBxI#Ro8Ml82lvP-lRKr9-Vkvkw");}
-function PackdeTemasOPL(){verificarSesionYAbrirEnlace("https://mega.nz/file/GEYmjLSb#3oM-f3S9bUC_lzjkxqpgbtMJzwZzw2exfSLm3obdSXI");}
-function XtremeEliteBoot(){verificarSesionYAbrirEnlace("https://mega.nz/folder/WR5EDC4Q#qCbxFTYubVRjdYPyXcVZgA");}
-function PS2SX(){verificarSesionYAbrirEnlace("https://mega.nz/folder/yNQlXYwR#qpnYEiDe_0bczjgp7s1cAg");}
-function PS2Temps(){verificarSesionYAbrirEnlace("https://mega.nz/folder/eNRgXYgI#G4kERdP2vvoDQEIVisyy0Q");}
+function Aplicaciones(){verificarSesionYAbrirEnlace("https://mega.nz/file/GE53VIgQ#g_No4H2bwJNwx341lHXO4VvXTD5wQB3I3OJGGB4px4E");}
+function MEGAPACKAPPSPS2(){verificarSesionYAbrirEnlace("https://mega.nz/file/vBpEFChI#kfHwrD6MSz0WKeHBwMgOkwXeKGi76fSVL0ZBNdRf8X8");}
+function PS2Temps(){verificarSesionYAbrirEnlace("https://mega.nz/folder/CRgV2KhT#X0NuceLpLABiPDczTNCqmQ");}
 function PS2TempsWeb(){verificarSesionYAbrirEnlace("https://www.psx-place.com/threads/ps2temps.27864/ ");}
-function PSXiSO(){verificarSesionYAbrirEnlace("https://mega.nz/folder/fJQxnA5D#ZE7ygsALpiGvInXhwG6hCw");}
-function PSXiSOWeb(){verificarSesionYAbrirEnlace("https://www.psx-place.com/threads/psxiso-compression-tool-v0-1-r4-chd-zso.37543/");}
-function wLaunchELFISR(){verificarSesionYAbrirEnlace("https://mega.nz/folder/SVhHXI7a#mc9oorr_fZLxvcf252NBAQ");}
+function wLaunchELFISR(){verificarSesionYAbrirEnlace("https://mega.nz/folder/2JwkEZoS#LVwtQw8MlxlubP24U2CqhA");}
 function wLaunchELFISRWeb(){verificarSesionYAbrirEnlace("https://github.com/israpps/wLaunchELF_ISR");}
+function PSXaPS2(){verificarSesionYAbrirEnlace("https://mega.nz/folder/TIBQhKgS#poYl08VUzSelt1F8c71bMw");}
+function Retroarch(){verificarSesionYAbrirEnlace("https://mega.nz/folder/uUIUkRYT#AyJT9Mz5BAEt7PsCqEDOtA");}
+function RetroarchWeb(){verificarSesionYAbrirEnlace("https://www.retroarch.com/?page=platforms");}
+function APPSUSB(){verificarSesionYAbrirEnlace("https://mega.nz/folder/jc5SBQhA#8m1jvxaTsEpjj9p59uwO3Q");}
+function OPLManager(){verificarSesionYAbrirEnlace("https://mega.nz/folder/7IYE3JQT#viGBs1qKb6Wzey-hG6l85A");}
+function OPLManagerWeb(){verificarSesionYAbrirEnlace("https://oplmanager.com/site/index.php");}
+function OPLSERVER(){verificarSesionYAbrirEnlace("https://mega.nz/folder/iAhAhbpD#s_n6rtINweFYGjc-H_5OdA");}
+function OPLSERVERWeb(){verificarSesionYAbrirEnlace("https://github.com/elmariolo/OPL-Server");}
+function PSXiSO(){verificarSesionYAbrirEnlace("https://mega.nz/folder/qA4ABR5Q#EndB6bpNyeAeZBNP6gTP5g");}
+function PSXiSOWeb(){verificarSesionYAbrirEnlace("https://www.psx-place.com/threads/psxiso-compression-tool-v0-1-r4-chd-zso.37543/");}
+function USBUTIL(){verificarSesionYAbrirEnlace("https://mega.nz/folder/jBx3QSRZ#Hsn4XZsSHdvyi-sZfNXREQ");}
+function PackdeTemasOPL(){verificarSesionYAbrirEnlace("https://mega.nz/folder/fVozRBTB#bKaEZdxyi7rbXdPhvwg9Hw");}
+function XtremeEliteBoot(){verificarSesionYAbrirEnlace("https://mega.nz/folder/vF5DlDgQ#XaER7fdgIrl7ApnsaTDgLA");}
+function PS2SX(){verificarSesionYAbrirEnlace("https://mega.nz/folder/GMIGzBxD#ZZaKjzsWlOkisa_sewocTQ");}
+
 
 
 
