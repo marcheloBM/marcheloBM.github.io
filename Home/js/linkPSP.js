ae7e0da02904bf9487f4f94ebb143ed2f2e8b569
Marcelo Burgos <marchelo.bm@gmail.com>
2025-10-10 05:01:10 -0300

    v2.14 Nueva lista de programas de consolas
    Actualizacion de la lista de programas para PSP y otros cambios

--- Home/js/linkPSP.js
+++ Home/js/linkPSP.js
@@ -1,27 +1,44 @@
 //Firmware oficial
 function OFW(){verificarSesionYAbrirEnlace("https://mega.nz/folder/OAp01LyB#2iME8GWTOBWEYPK69_OawQ");}
 //Hackear
-function CFW(){verificarSesionYAbrirEnlace("https://mega.nz/folder/7c5FwKLR#tdEqfRuInhfUYxXUro6Czg");}
-//Hombrew-Aplicaciones
+function CFW(){verificarSesionYAbrirEnlace("https://mega.nz/folder/WFJSUQbJ#qId5FYh-InrQeuNec0g2JQ");}
 function INFINITY(){verificarSesionYAbrirEnlace("https://mega.nz/folder/OdIT1QSA#Rhpxbhpvr9vrDMIfOM58mA");}
+//ARK4
+function ARK4(){verificarSesionYAbrirEnlace("https://mega.nz/folder/mIgDHRQa#biyYe0i1UU9GjVkCBhtHYQ");}
+function ARK4Web(){verificarSesionYAbrirEnlace("https://github.com/PSP-Archive/ARK-4");}
+//Hombrew-Aplicaciones
 function PSPPDA(){verificarSesionYAbrirEnlace("https://mega.nz/folder/OdIT1QSA#Rhpxbhpvr9vrDMIfOM58mA");}
+function PSPPDAWeb(){verificarSesionYAbrirEnlace("http://zx81.zx81.free.fr/serendipity/index.php?/archives/313-PSP-PDA-A-set-of-PDA-applications-for-PSP-v1.3.2.html");}
 function AirCrackPSP(){verificarSesionYAbrirEnlace("https://mega.nz/folder/uMxHxDbZ#8P0Qu2sV9zH5052KZzurgA");}
+function AirCrackPSPWeb(){verificarSesionYAbrirEnlace("https://www.gamebrew.org/wiki/AirCrack-PSP");}
 function Alarma(){verificarSesionYAbrirEnlace("https://mega.nz/folder/DUQHiCBZ#33v9jkSi4O_cacm8un3JHA");}
+function AlarmaWeb(){verificarSesionYAbrirEnlace("https://www.gamebrew.org/wiki/PSP_Hardware_Alarm_Suite_II");}
 function PSPOSB(){verificarSesionYAbrirEnlace("https://mega.nz/folder/7QZVxYhT#XU3VEQZ-r8MQGe-Ps7XkBw");}
 function PSPOSBWeb(){verificarSesionYAbrirEnlace("https://github.com/xerpi/psp-uvc-usb-video-class/releases");}
 function Chronoswitch(){verificarSesionYAbrirEnlace("https://mega.nz/folder/nYhTSD5Z#Qi5DIhd_ukk6_ie6O0z9eA");}
+function ChronoswitchWeb(){verificarSesionYAbrirEnlace("https://github.com/krazynez/Chronoswitch");}
 function CIPLFlasher(){verificarSesionYAbrirEnlace("https://mega.nz/folder/PM5yGaKI#PjSjDPghquA03tNoUBlrgg");}
 function FuSaGAMEPAD(){verificarSesionYAbrirEnlace("https://mega.nz/folder/CJIx2bpK#9Ns6v8ovWUwbboutvCEyjA");}
+function FuSaGAMEPADWeb(){verificarSesionYAbrirEnlace("https://github.com/andy-man/psp-fusa-gamepad");}
 function PSPLight(){verificarSesionYAbrirEnlace("https://mega.nz/folder/LMY3ybbZ#MvGjMHfX4HFOgkLftRgSEw");}
+function PSPLightWeb(){verificarSesionYAbrirEnlace("https://www.gamebrew.org/wiki/PSPLight");}
 function LIVETV(){verificarSesionYAbrirEnlace("https://mega.nz/folder/uFRxABrB#ZJXbQ8U8gChSC2vuBcgWqg");}
 function MapaSantiago(){verificarSesionYAbrirEnlace("https://mega.nz/folder/bd4nUKba#_RPvh83gFplMlhKqeBSnWA");}
 function CWCheat(){verificarSesionYAbrirEnlace("https://mega.nz/folder/aNw1hDKD#14cKh-EHmSsNC89wtGV3Yg");}
+function CWCheatWeb(){verificarSesionYAbrirEnlace("https://www.gamebrew.org/wiki/CWCheat_PSP");}
+function CWCheatBDWeb(){verificarSesionYAbrirEnlace("https://github.com/Saramagrean/CWCheat-Database-Plus-");}
 function NitePr(){verificarSesionYAbrirEnlace("https://mega.nz/folder/SNpUlZiJ#cB-ovP4fUYI8_JrWSHn4Mg");}
+function NitePrWeb(){verificarSesionYAbrirEnlace("https://www.gamebrew.org/wiki/NitePR_PSP");}
 function PSPRadio(){verificarSesionYAbrirEnlace("https://mega.nz/folder/zZAhTYqS#MDQmk2Kj7VFZG609ZvuhMw");}
+function PSPRadioWeb(){verificarSesionYAbrirEnlace("https://www.gamebrew.org/wiki/PSPRadio");}
 function PSPReader(){verificarSesionYAbrirEnlace("https://mega.nz/folder/XUJ2RYxJ#48n3DPad_hed4BobVYxc5w");}
 function Ubuntu(){verificarSesionYAbrirEnlace("https://mega.nz/folder/qc5wnDiB#ZqKPDc-O0eMZlNk0rkeGjw");}
 function Windows(){verificarSesionYAbrirEnlace("https://mega.nz/folder/eEoWTRTK#z1nRWn95WvrnLX-q3cguYQ");}
 function NavegadorHTML(){verificarSesionYAbrirEnlace("https://mega.nz/folder/Scoi1BwQ#PIEe_NMzpJbrT23brrD4OA");}
+function PKGi(){verificarSesionYAbrirEnlace("https://mega.nz/folder/rVgwiToJ#fHsdhPHSiyi59HtHL1oaAA");}
+function PKGiWeb(){verificarSesionYAbrirEnlace("https://github.com/bucanero/pkgi-psp");}
+function FTPD(){verificarSesionYAbrirEnlace("https://mega.nz/folder/fUYW1AjJ#FqFBsu0bmx5L40hFw-aNjQ");}
+function FTPDWeb(){verificarSesionYAbrirEnlace("https://github.com/PSP-Archive/PSP-FTPD");}
 //Emuladores
 function Atari(){verificarSesionYAbrirEnlace("https://mega.nz/folder/aQ5AQZBa#lXg_feiMilYYrH73wd_wLw");}
 function ColecoVision(){verificarSesionYAbrirEnlace("https://mega.nz/folder/qBJVHZBT#_wtYCVdFtzvnI2L6nzzaTg");}
@@ -40,7 +57,7 @@ function Temas(){verificarSesionYAbrirEnlace("https://mega.nz/folder/HAgQzRKR#Ze
 //PC
 function PSX2PSP(){verificarSesionYAbrirEnlace("https://mega.nz/folder/fEIinT7A#5r01qkjawl2PIteX0x3t8w");}
 function AllPSPWeb(){verificarSesionYAbrirEnlace("https://archive.org/details/psp-homebrew-library");}
-
+function SavesPS1PSP(){verificarSesionYAbrirEnlace("https://mega.nz/folder/KBAiERrA#mnx4TEm-aDJnT_Qi-09rOw");}
 
 
 
